96d0b757b4cac0a4f10507fcfc52e0f4f65a3a2e
igoride <igor_y_ide@hotmail.com>
2026-07-23 16:25:02 -0300

    feat: adiciona docker-compose e personalizacao do swagger ui

--- src/main/java/com/example/crud/configuration/OpenApiConfig.java
+++ src/main/java/com/example/crud/configuration/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.example.crud.configuration;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,11 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("CRUD")
+                        .version("1.0.1")
+                        .description("CRUD utilizando Spring Boot")
+                )
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
