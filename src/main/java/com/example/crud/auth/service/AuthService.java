fa27f1b94b5636271c88ecee35bdf98f89e2961b
igoride <igor_y_ide@hotmail.com>
2026-08-05 00:09:28 -0300

    ongoing

--- src/main/java/com/example/crud/auth/service/AuthService.java
+++ src/main/java/com/example/crud/auth/service/AuthService.java
@@ -1,5 +1,7 @@
-package com.example.crud.auth;
+package com.example.crud.auth.service;
 
+import com.example.crud.auth.dto.LoginDTO;
+import com.example.crud.auth.dto.LoginResponseDTO;
 import com.example.crud.user.User;
 
 public interface AuthService {
